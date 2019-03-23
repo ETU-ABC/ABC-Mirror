87ce381eecb5f25161a84cfddfec6909a69ee66a
bahadircolak44 <bahdrcolak@gmail.com>
2019-03-23 14:48:59 +0300

    add_new_user has been changed. It designed for server(endpoint) #26

--- modules/face_recognition/add_new_user.py
+++ modules/face_recognition/add_new_user.py
@@ -2,27 +2,41 @@ import cv2
 import time
 from imutils.video import VideoStream
 from imutils.video import FPS
+import argparse
 import os
+
+# Parse name
+parser = argparse.ArgumentParser()
+parser.add_argument("--name")
+args = vars(parser.parse_args())
+directory = args["name"]
+
+# Count for saving files
 i = 0
+
+# Video stream
 vs = VideoStream(src=0).start()
 fps = FPS().start()
-directory = input('Adinizi giriniz: ')
+
+
 print("[INFO] Merhaba {}.Gulumse cekiyorum".format(directory))
 time.sleep(0.5)
-if not os.path.exists("dataset/{}".format(directory)):
-    os.makedirs("dataset/{}".format(directory))
+
+if not os.path.exists("modules/face_recognition/dataset/{}".format(directory)):
+    os.makedirs("modules/face_recognition/dataset/{}".format(directory))
 oldtime = time.time()
 while time.time()-oldtime < 6:
     image = vs.read()
-    cv2.imwrite('dataset/{}/opencv'.format(directory)+str(i)+'.png', image)
+    cv2.imwrite('modules/face_recognition/dataset/{}/opencv'.format(directory)+str(i)+'.png', image)
     time.sleep(0.1)
     fps.update()
     i+=1
+
 fps.stop()
+
 print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
-print("[INFO] Fotograf cekimi tamamlandi. Encode islemi basliyor.")
-print("[INFO] Bu islem birkac saniye surecek.")
+print("[INFO] Fotograf cekimi tamamlandi")
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
